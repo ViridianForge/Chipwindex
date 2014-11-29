1774a3fca3d876407a9dc6941245546b4d461ade
Wayne Allen Manselle <viridianforge@gmail.com>
2014-11-28 22:58:28 -0800

    Changing up the database
    Switching up the database system to function off of a csv that's updated
    by....some mechanism

--- Libs/chipchart.js
+++ Libs/chipchart.js
@@ -1,7 +1,38 @@
 
+// Function to convert the CSV flat file into a json variable
+
+function csv2DTJson(csvData){
+
+	var lines=csv.split("\n");
+ 	var result = [];
+ 
+	var headers=lines[0].split(",");
+ 
+	for(var i=1;i<lines.length;i++){
+	
+		var obj = {};
+		var currentline=lines[i].split(",");
+ 
+		for(var j=0;j<headers.length;j++){
+			obj[headers[j]] = currentline[j];
+		}
+ 
+		result.push(obj); 
+
+	return json.stringify(result)
+}
+
 //This snippet here is where we actually render the table.
 
 $(document).ready(function() {
+
+	var csvDB = '';
+	
+	dbJSON = csv2DTJson(csvDB);
+
+	// We might need a touch right here to grab the size elements
+	// of the submission form and alter them to better match the screen
+
   $('#chipcharttable').DataTable({
     "ajax": {
       "url": "Database/Processed/chartList.json",
@@ -11,7 +42,9 @@ $(document).ready(function() {
     "columns": [
       { "title": "Album" },
       { "title": "Artist" },
-      { "title": "Release Date"}
+	  { "title": "Label" },
+      { "title": "Release Date"},
+	  { "title": "Art"}
     ]
     });
 });
