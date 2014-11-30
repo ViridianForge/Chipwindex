0911964725a0533a3d1e2c80b174287e1ab1a1ed
Wayne Allen Manselle <viridianforge@gmail.com>
2014-11-30 14:03:29 -0800

    Testing
    Testing Updates

--- Libs/chipchart.js
+++ Libs/chipchart.js
@@ -10,7 +10,11 @@ $(document).ready(function() {
 	//Grab the data from the publically available copy of the ChipChart master database
 	$.getJSON("https://spreadsheets.google.com/feeds/list/1fSwmSAB-qluTvy8YiTgunHKFbJsKMLg36xDNp8uhFIk/od6/public/values?alt=json",function(data){
 		//Test Print
-		console.log(data.feed.entry[0]['gsx$title']['$t']);
+		var chartData = [];
+		$.each(data.feed.entry, function(key, val){
+			console.log(key);
+			console.log(val);
+		});
 		console.log("Success.");
 	});
 
