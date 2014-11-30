67763a4c9da12904f7a7ed9accfd99505169234e
ViridianForge <viridianforge@gmail.com>
2014-11-30 15:51:39 -0800

    Precursor to Datatable direct from Sheets
    Base of the code to directly pull Datatable from Google Sheet.  Needs
    debugging

--- Libs/chipchart.js
+++ Libs/chipchart.js
@@ -6,30 +6,32 @@ $(document).ready(function() {
 	$("#subForm").width($(window).width()*0.2);
 	$("#subForm").height($(window).height());
 	
-	console.log("Attempting to get JSON.")
+	//console.log("Attempting to get JSON.")
 	//Grab the data from the publically available copy of the ChipChart master database
-	$.getJSON("https://spreadsheets.google.com/feeds/list/1fSwmSAB-qluTvy8YiTgunHKFbJsKMLg36xDNp8uhFIk/od6/public/values?alt=json",function(data){
+	//$.getJSON("https://spreadsheets.google.com/feeds/list/1fSwmSAB-qluTvy8YiTgunHKFbJsKMLg36xDNp8uhFIk/od6/public/values?alt=json",function(data){
 		//Test Print
-		var chartData = [];
-		$.each(data.feed.entry, function(key, val){
-			console.log(key);
-			console.log(val);
-		});
-		console.log("Success.");
+		//var chartData = [];
+		//$.each(data.feed.entry, function(key, val){
+		//	console.log(key);
+		//	console.log(val);
+		//});
+		//console.log("Success.");
 	});
 
+	//Major props on helping figure out populating a DataTables table from Google Sheets to raza
+	//from:  http://datatables.net/forums/discussion/5611/how-to-grab-datatables-data-from-a-google-spreadsheet
+	
   $('#chipcharttable').DataTable({
-    "ajax": {
-      "url": "Database/Processed/chartList.json",
-      "dataSrc":"chartData"
-    },
-    "order":[[2,"desc"]],
-    "columns": [
-      { "title": "Title" },
-      { "title": "Artist" },
-	  { "title": "Label" },
-      { "title": "Release Date"},
-	  { "title": "Available At"}
-    ]
+    "bServerSide":false,
+	"bProcessing":true,
+	"sAjaxDataProp":"feed.entry",
+	"sAjaxSource":"<a href="https://spreadsheets.google.com/feeds/list/1fSwmSAB-qluTvy8YiTgunHKFbJsKMLg36xDNp8uhFIk/od6/public/values?alt=json"" target="_blank" rel="nofollow">https://spreadsheets.google.com/feeds/list/1fSwmSAB-qluTvy8YiTgunHKFbJsKMLg36xDNp8uhFIk/od6/public/values?alt=json"</a>",
+	"aoColums": [
+		{ "mDataProp": "gsx$title.$t"},
+		{ "mDataProp": "gsx$artist.$t"},
+		{ "mDataProp": "gsx$label.$t"},
+		{ "mDataProp": "gsx$releasedate.$t"},
+		{ "mDataProp": "gsx$availableat.$t"}
+	]
     });
 });
