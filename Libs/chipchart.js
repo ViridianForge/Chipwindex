522dd3c3956960403b57451cefe696b5286a9ae8
Wayne Allen Manselle <viridianforge@gmail.com>
2014-11-30 23:02:28 -0800

    Clean up
    Removing unneeded comments.

--- Libs/chipchart.js
+++ Libs/chipchart.js
@@ -6,18 +6,6 @@ $(document).ready(function() {
 	$("#subForm").width($(window).width()*0.2);
 	$("#subForm").height($(window).height());
 	
-	//console.log("Attempting to get JSON.")
-	//Grab the data from the publically available copy of the ChipChart master database
-	//$.getJSON("https://spreadsheets.google.com/feeds/list/1fSwmSAB-qluTvy8YiTgunHKFbJsKMLg36xDNp8uhFIk/od6/public/values?alt=json",function(data){
-		//Test Print
-		//var chartData = [];
-		//$.each(data.feed.entry, function(key, val){
-		//	console.log(key);
-		//	console.log(val);
-		//});
-		//console.log("Success.");
-	//});
-
 	//Major props on helping figure out populating a DataTables table from Google Sheets to raza
 	//from:  http://datatables.net/forums/discussion/5611/how-to-grab-datatables-data-from-a-google-spreadsheet
 	
