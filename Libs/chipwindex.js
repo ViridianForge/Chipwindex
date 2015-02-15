a37be4bb0a7b2af83f3c276b3a4b3466e608ffaa
Wayne Allen Manselle <viridianforge@gmail.com>
2015-02-15 00:01:46 -0800

    More graphical updates
    Progress!

--- Libs/chipwindex.js
+++ Libs/chipwindex.js
@@ -11,9 +11,26 @@ $(document).ready(function() {
 		autoOpen: false,
 		modal: true,
 		maxWidth: $(window).width()*0.3,
-		maxHeight: $(window).width()*0.8,
+		maxHeight: $(window).height()*0.8,
 		width: $(window).width()*0.3,
-		height: $(window).width()*0.8,
+		height: $(window).height()*0.8,
+		show: {
+			effect: "slide",
+			duration: 333
+		},
+		hide: {
+			effect: "slide",
+			duration: 333
+		}
+	});
+	
+	$("#aboutDialog").dialog({
+		autoOpen: false,
+		modal: true,
+		maxWidth: $(window).width()*0.3,
+		maxHeight: $(window).height()*0.8,
+		width: $(window).width()*0.3,
+		height: $(window).height()*0.8,
 		show: {
 			effect: "slide",
 			duration: 333
@@ -25,23 +42,20 @@ $(document).ready(function() {
 	});
 		
 	//Prep the button to open the iframe
-	$("#dialogButton").click(function(){
+	$("#submitButton").click(function(){
 		$("#subDialog").dialog("open");
+	});
+	
+	$("#aboutButton").click(function(){
+		$("#aboutDialog").dialog("open");
 	});
 	
 	//Major props on helping figure out populating a DataTables table from Google Sheets to raza
 	//from:  http://datatables.net/forums/discussion/5611/how-to-grab-datatables-data-from-a-google-spreadsheet
 	
-	//Original sAjaxSource
-	//"<a href="https://spreadsheets.google.com/feeds/list/1fSwmSAB-qluTvy8YiTgunHKFbJsKMLg36xDNp8uhFIk/od6/public/values?alt=json"" target="_blank" rel="nofollow">https://spreadsheets.google.com/feeds/list/1fSwmSAB-qluTvy8YiTgunHKFbJsKMLg36xDNp8uhFIk/od6/public/values?alt=json"</a>",
-	
   $('#chipcharttable').DataTable({
     "bServerSide":false,
 	"bProcessing":true,
-	"iDisplayLength":15,
-	"language":{
-		"lengthMenu": "Display <select><option value='10'>10</option><option value='15'>15</option><option value='30'>30</option></select> records per page"
-	},
 	"sAjaxDataProp":"feed.entry",
 	"sAjaxSource":"https://spreadsheets.google.com/feeds/list/1fSwmSAB-qluTvy8YiTgunHKFbJsKMLg36xDNp8uhFIk/od6/public/values?alt=json",
 	"aoColumns":[
@@ -57,7 +71,10 @@ $(document).ready(function() {
 		{ "title": "Available At", "mData": "gsx$availableat.$t"},
 		{ "title": "Genre", "mData": "gsx$genre.$t", "visible":false}
 	],
-	"aaSorting":[[7,"desc"]]
+	"aaSorting":[[7,"desc"]],
+	"sScrollY": Math.floor(($(window).height())*0.65),
+	"bPaginate": false,
+	"info": false
     });
 });
 
